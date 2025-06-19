8afff472e3a3728607cca06a311cbc7d18fae2ad
ZOASR <44146518+ZOASR@users.noreply.github.com>
2025-06-19 21:25:17 +0300

    dev: added cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,18 @@ import icon from "astro-icon";
 
 import solidJs from "@astrojs/solid-js";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
+
 	vite: {
 		plugins: [tailwindcss()],
 	},
 
 	integrations: [icon(), solidJs()],
+	adapter: cloudflare({
+		imageService: "cloudflare",
+	}),
 });
